c3095138ae5a95519520c4934cdaca58fcf732a6
adalberto-jr7 <adalberto.garcia@administracionisc.gob.mx>
2024-09-05 14:14:25 -0700

    feat: modify index and routes changed

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class, 'index'])->name('home') ;
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
