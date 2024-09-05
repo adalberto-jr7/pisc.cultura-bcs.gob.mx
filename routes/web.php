270f3ef396e1d27071ddb0d3b84349d08f86ca16
adalberto-jr7 <adalberto.garcia@administracionisc.gob.mx>
2024-09-05 15:32:10 -0700

    feat: add route for the dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [HomeController::class, 'index'])->name('home') ;
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Auth::routes(['register' => false]);
 
+Route::get('/admin/dashboard', function () {
+    if (Gate::allows('isAdmin')) {
+        return view('dashboard');
+    } else {
+        return "Not Authorized";
+    }
+})->middleware('auth');
