6c6f7fe9ab9ad09040d64789bae6c2d528ce74c0
Gabriel Casillas <luca_20@alu.uabcs.mx>
2024-09-11 14:31:41 -0700

    Correccion comentarios login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="container d-flex align-items-start mt-3 " @style(['width: 850px'])>
-    <!-- <img src="{{ asset('assets/bcs.jpg') }}"> -->
+    
     <div class="row justify-content-center mt-0">
 
         <div class="d-flex justify-content-center mt-5">
@@ -18,7 +18,7 @@
         </div>
 
 
-        <!-- <img src="{{ asset('assets/logo-1.jpg') }}" @style(['width: 450px'])> -->
+        
         <div class="col-md-8 d-flex justify-content-center mt-4" @style(['width: 850px'])>
             <div class="card" @style(['width: 500px'])>
                 <div class="card-header d-flex justify-content-center text-white" @style(['background-color: #9F2241'])>{{ __('Iniciar Sesion') }}</div>
@@ -85,37 +85,6 @@
                 </div>
             </div>
         </div>
-
-
-        <!-- <div class="container overflow-hidden text-center">
-            <div class="row gx-0">
-                <div class="col-4">
-                    <div class="p-4">
-                    <img src="{{ asset('assets/logo-1.jpg') }}" @style(['width: 450px'])>
-                    </div>
-                </div>
-                <div class="col-8">
-                    <div class="p-3">
-                    <img src="{{ asset('assets/ISC30N24.jpg') }}" @style(['width: 95px'])>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-
-
-        <!-- prueba  -->
-        <!-- <div class="d-flex justify-content-center mt-5">
-            <div class="row">
-                <div class="col-10">
-                    <img src="{{ asset('assets/logo-1.jpg') }}" @style(['width: 450px'])>
-                </div>
-                <div class="col-2 mb-2">
-                    <img src="{{ asset('assets/ISC30N24.jpg') }}" @style(['width: 95px'])>
-                </div>
-            </div>
-        </div>
-
-        <!-- <img src="{{ asset('assets/logo-1.jpg') }}" @style(['width: 450px'])> -->
     </div>
 </div> 
 
