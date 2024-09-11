be7082437398f2af0bd274198fe9001bd8cbc971
Gabriel Casillas <luca_20@alu.uabcs.mx>
2024-09-11 15:04:11 -0700

    FillTables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('Proyectos Instituto Sudcaliforniano de Cultura', 'Proyectos Instituto Sudcaliforniano de Cultura') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
