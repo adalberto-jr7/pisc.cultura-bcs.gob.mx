baecf5c34e93f48b1e4a2f9169b4120c4a968c89
Gabriel Casillas <luca_20@alu.uabcs.mx>
2024-09-18 14:17:24 -0700

    Correcciones ortograficas del login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,7 +62,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Recuerdame') }}
+                                        {{ __('Recuérdame') }}
                                     </label>
                                 </div>
                             </div>
@@ -76,7 +76,7 @@
 
                                 @if (Route::has('password.request'))
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('¿Olvidaste tu contraseña?') }}
+                                    {{ __('¿Olvidaste tú contraseña?') }}
                                 </a>
                                 @endif
                             </div>
